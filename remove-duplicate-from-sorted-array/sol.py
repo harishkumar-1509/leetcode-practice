221865bcf77aa25f67b0ed272d706c2357ae48d1
harishkumar-1509 <harishgandhi1509@gmail.com>
2024-05-29 00:09:01 +0530

    remove duplicate from sorted

---
+++ remove-duplicate-from-sorted-array/sol.py
@@ -0,0 +1,13 @@
+def removeDuplicates(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    if not nums:
+        return 0
+    k=1
+    for i in range(1,len(nums)):
+        if nums[i]!=nums[i-1]:
+            nums[k]=nums[i]
+            k+=1
+    return k
